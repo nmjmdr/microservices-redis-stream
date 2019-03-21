a91ab44ccda5325d5fe3808b56f2bbb4e31145c4
nmjmdr <narasimha.gm@gmail.com>
2019-03-22 00:11:46 +1100

    in progress - sync

--- account-service/src/synchronizer/sync.go
+++ account-service/src/synchronizer/sync.go
@@ -4,3 +4,24 @@ type Sync interface {
 	Start() error
 	Stop()
 }
+type sync struct {
+	stop chan bool
+}
+
+func NewSync() Sync {
+	return &sync{
+		stop: make(chan bool),
+	}
+}
+
+func (s *sync) Start() error {
+	select {
+	case <-s.stop:
+		s.quit
+	}
+	return nil
+}
+
+func (s *sync) Stop() {
+
+}
