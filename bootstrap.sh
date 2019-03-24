731e177e4093bcab21acfe67f588d8756f5e8e54
nmjmdr <narasimha.gm@gmail.com>
2019-03-24 20:08:53 +1100

    bootstrap delay

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,24 @@
 # create network
 docker network create net
 
+# start db-host
+cd db-host
+docker-compose up --build &
+
+# Ideally should poll the container to check if its up
+sleep 10
+
 # start redis container
+cd ..
 cd redis-host
 docker-compose up --build &
 
+# Ideally should poll the container to check if its up
+sleep 10
 
-# start db-host
+# start customer-service
 cd ..
-cd db-host
+cd customer-service
 docker-compose up --build &
 
 # start account-service
@@ -18,8 +28,5 @@ cd ..
 cd account-service
 docker-compose up --build &
 
-# start customer-service
-cd ..
-cd customer-service
-docker-compose up --build &
+
 
